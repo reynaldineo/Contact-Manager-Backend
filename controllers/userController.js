647b60511c1dec5f6cd527e38ea1ec6fd1404e2e
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-24 21:33:20 +0700

    feat: add user registration with password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,43 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 //@desc Register user
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // hass password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`user created: ${user}`);
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
   res.json({ message: "Register user" });
 });
 
