48cd05b58036469d9e9426f49cc8704ce3f69458
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-24 21:49:57 +0700

    feat: add user login with JWT acces token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 //@desc Register user
 //@route POST /api/users/register
@@ -45,7 +46,33 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+
+  const user = await User.findOne({ email });
+  //   compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accesToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id,
+        },
+      },
+      process.env.ACCES_TOKEN_SECRET,
+      {
+        expiresIn: "2m",
+      }
+    );
+    res.status(200).json({ accesToken });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Current user info
