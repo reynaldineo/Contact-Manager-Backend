9f12b7ddfd15eefbe5179140636825c7f3997faf
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-24 22:24:43 +0700

    feat: add verify JWT token middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -79,7 +79,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
